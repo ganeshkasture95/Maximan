0002e79925acf96385d7f22b35b26fcec8af1991
Ganesh Kasture <ganeshkasture9595@gmail.com>
2024-12-29 13:47:03 +0530

    Authentication completed

---
+++ android/app/src/main/kotlin/com/gkmatrix/maximan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gkmatrix.maximan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:maximan/firebase_options.dart';
+import 'package:maximan/views/register_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+      useMaterial3: true,
+    ),
+    home: const MyApp(),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: const Text('Maximan'),
+      ),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user == null) {
+                return const RegisterView();
+              }
+              return const Text("Done");
+            default:
+              return const Text("Loading...");
+          }
+        },
+      ),
+    );
+  }
+}
